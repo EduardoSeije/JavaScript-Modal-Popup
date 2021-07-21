078f4f644f7369432f5f3ebdf3d95c66bb6bd604
Eduardo Seije <eduardoseije@gmail.com>
2021-07-21 11:27:44 -0300

    Start the JavaScript. Select all the elements we need to the application

---
+++ script.js
@@ -0,0 +1,3 @@
+const openModalBtn = document.querySelectorAll('[data-modal-target]');
+const closeModalBtn = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
